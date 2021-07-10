c6409ca007b28bb0f2db311bac14ca5c59a9123a
DenysSemanchuk1 <semanchukdenys@gmail.com>
2021-07-10 20:20:04 +0300

    added advantages and author sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
   var mixer = mixitup('.products__inner-box');
+
+  $(".rateyo").rateYo({
+    rating: 5,
+    readOnly: true,
+    ratedFill: '#ffa726',
+    starWidth: "12px",
+  });
+
+  $('.products-slider__box').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
 });
