e986c2682ace6ff13d375b7e3906c21b2dbee80a
DenysSemanchuk1 <semanchukdenys@gmail.com>
2021-07-27 11:39:06 +0300

    adaptive header and footer

--- gulpfile.js
+++ gulpfile.js
@@ -9,41 +9,46 @@ let cssmin = require("gulp-cssmin");
 
 gulp.task("sass", function () {
   return gulp
-      .src("app/scss/**/*.scss")
-      .pipe(sass({outputStyle: "compressed"}))
-      .pipe(rename({suffix: ".min"}))
-      .pipe(
-          autoprefixer({
-            overrideBrowserslist: ["last 8 versions"],
-          })
-      )
-      .pipe(gulp.dest("app/css"))
-      .pipe(
-          browserSync.reload({
-            stream: true,
-          })
-      );
+    .src("app/scss/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 8 versions"],
+      })
+    )
+    .pipe(gulp.dest("app/css"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
 });
 gulp.task("style", function () {
   return gulp
-      .src([
-        "node_modules/normalize.css/normalize.css",
-        "node_modules/slick-carousel/slick/slick.css",
-      ])
-      .pipe(concat("libs.min.css"))
-      .pipe(cssmin())
-      .pipe(gulp.dest("app/css"));
+    .src([
+      "node_modules/normalize.css/normalize.css",
+      "node_modules/slick-carousel/slick/slick.css",
+      "node_modules/ion-rangeslider/css/ion.rangeSlider.css",
+      "bower_components/jquery.form-styler/dist/jquery.formstyler.css",
+      "bower_components/jquery.form-styler/dist/jquery.formstyler.theme.css",
+    ])
+    .pipe(concat("libs.min.css"))
+    .pipe(cssmin())
+    .pipe(gulp.dest("app/css"));
 });
 gulp.task("script", function () {
   return gulp
-      .src([
-        "node_modules/slick-carousel/slick/slick.js",
-        "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
-        "node_modules/mixitup/dist/mixitup.js",
-      ])
-      .pipe(concat("libs.min.js"))
-      .pipe(uglify())
-      .pipe(gulp.dest("app/js"));
+    .src([
+      "node_modules/slick-carousel/slick/slick.js",
+      "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
+      "node_modules/mixitup/dist/mixitup.js",
+      "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+      "bower_components/jquery.form-styler/dist/jquery.formstyler.js",
+    ])
+    .pipe(concat("libs.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("app/js"));
 });
 gulp.task("browser-sync", function () {
   browserSync.init({
@@ -54,16 +59,16 @@ gulp.task("browser-sync", function () {
 });
 gulp.task("html", function () {
   return gulp.src("app/*.html").pipe(
-      browserSync.reload({
-        stream: true,
-      })
+    browserSync.reload({
+      stream: true,
+    })
   );
 });
 gulp.task("js", function () {
   return gulp.src("app/js/*.js").pipe(
-      browserSync.reload({
-        stream: true,
-      })
+    browserSync.reload({
+      stream: true,
+    })
   );
 });
 
@@ -74,6 +79,6 @@ gulp.task("watch", function () {
 });
 
 gulp.task(
-    "default",
-    gulp.parallel("style", "script", "sass", "watch", "browser-sync")
+  "default",
+  gulp.parallel("style", "script", "sass", "watch", "browser-sync")
 );
